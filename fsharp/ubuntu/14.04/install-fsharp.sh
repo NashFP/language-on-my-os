8cc0c688a3840b4c25f019a5c590386d2317ba72
Nate West <natescott.west@gmail.com>
2014-03-26 00:48:51 -0500

    F# install script additional packages

--- fsharp/ubuntu/14.04/install-fsharp.sh
+++ fsharp/ubuntu/14.04/install-fsharp.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get install fsharp monodevelop
+sudo apt-get update && sudo apt-get install fsharp monodevelop mono-xsp4 asp.net-examples monodevelop-nunit monodevelop-versioncontrol monodevelop-database
